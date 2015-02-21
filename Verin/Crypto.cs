ef3adaae009c55a29fd04e25e2ea4896c6f5afb7
Javier Rico <javi921@hotmail.es>
2015-02-21 20:04:19 +0100

    Added Project files

---
+++ Verin/Crypto.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verin
+{
+    static class Crypto
+    {
+        public static String getHash(FileStream stream, Folder.HashType hashType)
+        {
+            String hexHash;
+
+            switch (hashType)
+            {
+                case Folder.HashType.MD5:
+                {
+
+                }
+
+            }
+        }
+
+        public static String getMD5Hash(FileStream stream)
+        {
+            String hexHash;
+
+            using (MD5 md5hash = MD5.Create())
+            {
+                byte[] rawHash = md5hash.ComputeHash(stream);
+
+                hexHash = hashToString(rawHash);
+            }
+
+            return hexHash;
+        }
+
+        public static String getRIPEMD160Hash(FileStream stream)
+        {
+            String hexHash;
+
+            using (RIPEMD160 ripemd160hash = RIPEMD160.Create())
+            {
+                byte[] rawHash = ripemd160hash.ComputeHash(stream);
+
+                hexHash = hashToString(rawHash);
+            }
+
+            return hexHash;
+        }
+
+        public static String getSHA1Hash(FileStream stream)
+        {
+            String hexHash;
+
+            using (SHA1 sha1hash = SHA1.Create())
+            {
+                byte[] rawHash = sha1hash.ComputeHash(stream);
+
+                hexHash = hashToString(rawHash);
+            }
+
+            return hexHash;
+        }
+
+        public static String getSHA256Hash(FileStream stream)
+        {
+            String hexHash;
+
+            using (SHA256 sha256hash = SHA256.Create())
+            {
+                byte[] rawHash = sha256hash.ComputeHash(stream);
+
+                hexHash = hashToString(rawHash);
+            }
+
+            return hexHash;
+        }
+
+        public static String getSHA384Hash(FileStream stream)
+        {
+            String hexHash;
+
+            using (SHA384 sha384hash = SHA384.Create())
+            {
+                byte[] rawHash = sha384hash.ComputeHash(stream);
+
+                hexHash = hashToString(rawHash);
+            }
+
+            return hexHash;
+        }
+
+        public static String getSHA512Hash(FileStream stream)
+        {
+            String hexHash;
+
+            using (SHA512 sha512hash = SHA512.Create())
+            {
+                byte[] rawHash = sha512hash.ComputeHash(stream);
+
+                hexHash = hashToString(rawHash);
+            }
+
+            return hexHash;
+        }
+
+        private static String hashToString(byte[] rawHash)
+        {
+            StringBuilder stringbuilder = new StringBuilder();
+
+            for (int i = 0; i < rawHash.Length; i++)
+            {
+                stringbuilder.Append(rawHash[i].ToString("x2"));
+            }
+            return stringbuilder.ToString();
+        }
+    }
+}
---
+++ Verin/Folder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verin
+{
+    class Folder
+    {
+        public enum HashType { MD5, RIPEMD160, SHA1, SHA256, SHA384, SHA512 };
+        private String name;
+        private String path;
+        private HashType hashType;
+
+        public Folder(String name, String path, HashType hashType)
+        {
+            this.name = name;
+            this.path = path;
+            this.hashType = hashType;
+        }
+
+        public void computeHashes()
+        {
+
+        }
+    }
+}
